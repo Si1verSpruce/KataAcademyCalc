e6c4b5d96fa7b9d8f514d314e752c6997cbea034
Elisey <eliseya2000@gmail.com>
2024-06-14 21:42:23 +0400

    added out of lower sequences exceptions, redundant lower chars exceptions, out of equal sequences exceptions

--- src/Roman.java
+++ src/Roman.java
@@ -1,5 +1,8 @@
 class Roman {
-    static final RomanValue[] ROMAN_VALUES = {
+    public static final int MAX_LOWER_NUMBERS_SEQUENCE = 1;
+    public static final int MAX_EQUAL_NUMBERS_SEQUENCE = 3;
+
+    public static final RomanValue[] ROMAN_VALUES = {
             new RomanValue("C", 100),
             new RomanValue("XC", 90),
             new RomanValue("L", 50),
@@ -15,16 +18,41 @@ class Roman {
         char[] chars = value.toCharArray();
         int maxNumber = 0;
         int sum = 0;
+        int lowerSequence = 0;
+        int equalSequence = 1;
+        int lastValue = 0;
 
         for (int i = chars.length - 1; i >= 0; i--) {
             int charValue = romanDigitToArabic(chars[i]);
 
             if (charValue >= maxNumber) {
+                if (charValue == maxNumber) {
+                    equalSequence++;
+
+                    if (equalSequence > MAX_EQUAL_NUMBERS_SEQUENCE) {
+                        throw new Exception();
+                    }
+                } else {
+                    equalSequence = 1;
+                }
+
+                lowerSequence = 0;
                 maxNumber = charValue;
                 sum += charValue;
             } else {
+                if (lastValue - charValue == charValue){
+                    throw new Exception();
+                }
+
                 sum -= charValue;
+                lowerSequence++;
+
+                if (lowerSequence > MAX_LOWER_NUMBERS_SEQUENCE) {
+                    throw new Exception();
+                }
             }
+
+            lastValue = charValue;
         }
 
         return sum;
@@ -33,7 +61,7 @@ class Roman {
     public static int romanDigitToArabic(char digit) throws Exception {
         String romanDigit = String.valueOf(digit);
 
-        for (RomanValue romanValue:ROMAN_VALUES){
+        for (RomanValue romanValue : ROMAN_VALUES) {
             if (romanValue.string.equals(romanDigit))
                 return romanValue.value;
         }
